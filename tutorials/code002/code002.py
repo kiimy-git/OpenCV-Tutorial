5ced1bb646d346659e72ba1d9e48209c7864e87b
kimmy <shinkew@gmail.com>
2021-12-08 16:00:57 +0900

    rotated, flip

--- tutorials/code002/code002.py
+++ tutorials/code002/code002.py
@@ -11,5 +11,16 @@ center = (w //2, h//2)
 M = cv.getRotationMatrix2D(center, -45, 1)
 rotated = cv.warpAffine(img, M, (w,h))
 
+
+# image flip
+flip = cv.flip(img, 0)
+'''
+flipCode < 0은 XY 축 대칭(상하좌우 대칭)
+
+flipCode = 0은 X 축 대칭(상하 대칭)
+
+flipCode > 0은 Y 축 대칭(좌우 대칭)
+'''
 cv.imshow('rotated', rotated)
+cv.imshow('flip', flip)
 cv.waitKey()
