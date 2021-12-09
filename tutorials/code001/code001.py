1904157c5c21f09fae888d63e4c2ec0592b524d9
kimmy-git <83389640+kimmy-git@users.noreply.github.com>
2021-12-09 13:21:52 +0900

    load image

--- tutorials/code001/code001.py
+++ tutorials/code001/code001.py
@@ -6,6 +6,8 @@ img = cv.imread('cat.bmp')
 print(img.shape)
 
 cv2.namedWindow("image", cv2.WINDOW_AUTOSIZE)
+# WINDOW_NORMAL = 영상 크기가 클때(마우스로 크기를 줄이거나 키울 수 있음)
+# WINDOW_AUTOSIZE = default
 cv.imshow('image', img)
 cv.waitKey()
 cv2.destroyAllWindows()
