cf7a14cef1208b0b6a37b2eb6753ab1fb6073ee4
kimmy-git <83389640+kimmy-git@users.noreply.github.com>
2021-12-09 12:38:30 +0900

    Update code003.py

--- tutorials/code003/code003.py
+++ tutorials/code003/code003.py
@@ -20,7 +20,7 @@ img = cv.imread('.\\ch01\\cat.bmp')
 dst = cv.pyrUp(img, (w*2, h*2), borderType= cv.BORDER_DEFAULT)
 dst2= cv.pyrDown(img)
 
-# draw image
+# draw image + linetype = cv.LINE_AA = 라인을 더 부드럽게 그림
 output = img.copy()
 # cv.rectangle(img, 시작 좌표(pt1)부터 도착 좌표(pt2), color, 두께)
 # pt1, pt2 X ==> rec 좌표 설정시(x, y, w, h)
@@ -29,7 +29,7 @@ rect = cv.rectangle(output, (300,20), (420,100), (0,0,255), 1)
 # cv.circle(img, '중심점', '반지름', color, 두께= -1 공간채움)
 circle = cv.circle(output, (h//2, w//2), 20, (0,255,0), -1)
 
-# cv.line(img, 시작 좌표(pt1)부터 도착 좌표(pt2), color, 두께) + linetype = cv.LINE_AA = 라인을 
+# cv.line(img, 시작 좌표(pt1)부터 도착 좌표(pt2), color, 두께)
 line = cv.line(output, (60, 20), (400, 200), (0, 255, 255), 5)
 
 # cv.putText(img, text, '좌측 상단 모서리(org)', fontFace, fontScale, color)
