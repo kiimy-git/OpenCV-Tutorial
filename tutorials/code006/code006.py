f63bbb0300911c7e5f7f915a09f3e2ecbb125a3f
kimmy <shinkew@gmail.com>
2021-12-11 14:11:35 +0900

    bitwise_and

--- tutorials/code006/code006.py
+++ tutorials/code006/code006.py
@@ -11,6 +11,7 @@ img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 ret, dst = cv.threshold(img_gray, 225, 250, cv.THRESH_BINARY_INV)
 
 mask = dst.copy()
+# mask 지정을 해줘야함
 output = cv.bitwise_and(img, img, mask=mask)
 
 cv.imshow('image', img)
